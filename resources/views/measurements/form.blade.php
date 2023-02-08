2ae7eb29eb009e44d206038022475d1d42d2c2fe
Tavinho <octavior.barassa@hotmail.com>
2023-02-08 15:54:08 -0300

    Diminuições e alterações no css principal.

--- resources/views/measurements/form.blade.php
+++ resources/views/measurements/form.blade.php
@@ -4,7 +4,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top py-1" id="mainNav">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand text-white" href="/home">Seja bem-vindo(a), {{auth()->user()->name}}</a>
+            <a class="navbar-brand text-white">Seja bem-vindo(a), {{auth()->user()->name}}</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto my-2 my-lg-0">
